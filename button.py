1e8d847571f9dcd8544ff3ee394f19cc26e1d549
Daniil Tsioma <dev.dtsioma@gmail.com>
2024-01-31 13:58:48 -0800

    keyboard letters rendered

---
+++ button.py
@@ -0,0 +1,43 @@
+# libraries
+import pygame
+
+black = pygame.Color(0, 0, 0)
+white = pygame.Color(255, 255, 255)
+green = pygame.Color(108, 169, 101)
+yellow = pygame.Color(200, 182, 83)
+gray = pygame.Color(120, 124, 127)
+lightgray = pygame.Color(230, 232, 234)
+
+
+class Button():
+    themes = [
+        (lightgray, black),             # default
+        (green, white),                 # green
+        (yellow, white),                # yellow
+        (gray, white),                  # gray
+    ]
+
+    def __init__(self, window, text, theme, x, y, width, height, font):
+        self.text = text
+        self.window = window
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.theme = self.themes[theme]
+        self.font = font
+
+    def render(self, window):
+        pygame.draw.rect(window, self.theme[0], [
+                         self.x, self.y, self.width, self.height])
+
+        # render text in this font
+        text = self.font.render(self.text, True, self.theme[1])
+
+        text_width = text.get_width()
+        text_height = text.get_height()
+
+        text_left = self.x + (self.width - text_width) / 2
+        text_top = self.y + (self.height - text_height) / 2
+
+        window.blit(text, (text_left, text_top))
--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-
+# libraries
 import pygame
 
+# components
+from button import Button
+
 # set window dimensions
-window_x = 480
-window_y = 720
+window_x = 265
+window_y = 600
+
+# set screen refresh rate
+screen_update = 30
 
 # set colors
 black = pygame.Color(0, 0, 0)
@@ -11,13 +17,44 @@ white = pygame.Color(255, 255, 255)
 green = pygame.Color(108, 169, 101)
 yellow = pygame.Color(200, 182, 83)
 gray = pygame.Color(120, 124, 127)
+lightgray = pygame.Color(230, 232, 234)
 
 # initialize pygame
 pygame.init()
 
+# button font
+smallfont = pygame.font.SysFont('times new roman', 20)
+
+
 # initialize pygame window
 game_window = pygame.display.set_mode((window_x, window_y))
 pygame.display.set_caption("Wordle")
+
+# refresh rate
+fps = pygame.time.Clock()
+
+letters = 'qwertyuiop'
+buttons: list[Button] = []
+
+offset_left = 10
+for ltr in letters:
+    buttons.append(Button(game_window, ltr, 0, offset_left,
+                   200, 20, 30, smallfont))
+    offset_left += 25
+
+letters = 'asdfghjkl'
+offset_left = 25
+for ltr in letters:
+    buttons.append(Button(game_window, ltr, 0, offset_left,
+                   235, 20, 30, smallfont))
+    offset_left += 25
+
+letters = 'zxcvbnm'
+offset_left = 50
+for ltr in letters:
+    buttons.append(Button(game_window, ltr, 0, offset_left,
+                   270, 20, 30, smallfont))
+    offset_left += 25
 
 running = True
 while running:
@@ -26,5 +63,14 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    pygame.display.flip()
+    # q_button.render(game_window)
+    for btn in buttons:
+        btn.render(game_window)
+
+    mouse = pygame.mouse.get_pos()
+
+    pygame.display.update()
+
+    fps.tick(screen_update)
+
 pygame.quit()
