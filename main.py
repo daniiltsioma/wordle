9bdb2c6a1ce9e9961a424300577da136b1b648c7
Daniil Tsioma <dev.dtsioma@gmail.com>
2024-02-01 16:50:04 -0800

    Squares class added

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pygame
 # components
 from button import Button
 from keyboard import Keyboard
+from squares import Squares
 
 # set window dimensions
 window_x = 265
@@ -31,22 +32,16 @@ pygame.display.set_caption("Wordle")
 # refresh rate
 fps = pygame.time.Clock()
 
+# initialize input squares
+squares = Squares(game_window)
+# draw squares
+squares.draw()
 
 # initialize keyboard
 keyboard = Keyboard(game_window)
 # create keyboard buttons
 keyboard.create()
 
-squares = []
-
-offset_top = 20
-for i in range(6):
-    offset_left = 10
-    for j in range(5):
-        squares.append(pygame.draw.rect(game_window, gray,
-                                        (offset_left, offset_top, 41, 41), 1))
-        offset_left += 51
-    offset_top += 51
 
 running = True
 while running:
